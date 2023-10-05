19b79701000fcc1fbb38d127937a6262c4c05852
Xuan <leeyitsuen0617@gmail.com>
2023-10-05 15:44:31 +0800

    modify

--- index.py
+++ index.py
@@ -4,4 +4,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.jinja.html")
+    names = ['1','2','3','4','5']
+    return render_template("index.jinja.html",names = names)
+
+@app.route("/feature")
+def feature():
+    return render_template("feature.jinja.html")
+
+@app.route("/description")
+def description():
+    return render_template("description.jinja.html")
+
+@app.route("/product")
+def product():
+    return render_template("product.jinja.html")
